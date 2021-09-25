655ff6f692e59fdf1fbee4c31eddebcd4c409552
joshuajong1 <90546610+joshuajong1@users.noreply.github.com>
2021-09-25 21:54:52 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,48 @@
+
+var vg_1 = "bump_chart.vg.json";
+vegaEmbed("#bump_chart", vg_1).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "donut_chart.vg.json";
+vegaEmbed("#donut_chart", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "word_cloud.vg.json";
+vegaEmbed("#word_cloud", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "map.vg.json";
+vegaEmbed("#map", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "ring_chart.vg.json";
+vegaEmbed("#radial_plot", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "concat_heatmap.vg.json";
+vegaEmbed("#heatmap-concat", vg_7).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "heatmap_cases.vg.json";
+vegaEmbed("#heatmap-cases", vg_8).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "deaths_isotype.vg.json";
+vegaEmbed("#isotype-death", vg_9).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vega.setRandom(vega.randomLCG1(2));
+
